3d4e179cc458082e54f57cfb641a2d33e0dd8303
imertz <imertz@protonmail.com>
2018-07-14 00:36:19 +0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import routes from './routes/'
+
+const app = express();
+const port = process.env.PORT || 5656;
+// routes go here
+
+app.use('/', routes)
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`)
+})
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+const router = express.Router()
+
+import db from '../queries'
+
+
+
+router.get('/api/puppies', db.getAllPuppies);
+router.get('/api/puppies/:id', db.getSinglePuppy);
+router.post('/api/puppies', db.createPuppy);
+router.put('/api/puppies/:id', db.updatePuppy);
+router.delete('/api/puppies/:id', db.removePuppy);
+
+export default router
