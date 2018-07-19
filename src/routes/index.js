0ab03e1d7f2e40cdd435f228a2128e3dfe6338da
Ioannis Mertzanis <jomertz@gmail.com>
2018-07-19 16:54:55 +0300

    Fixed routes /api

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,17 +5,17 @@ import db from "../queries.js";
 import Clients from "../queries/Clients";
 
 // Puppies Routes
-router.get("/api/puppies", db.getAllPuppies);
-router.get("/api/puppies/:id", db.getSinglePuppy);
-router.post("/api/puppies", db.createPuppy);
-router.put("/api/puppies/:id", db.updatePuppy);
-router.delete("/api/puppies/:id", db.removePuppy);
+router.get("/puppies", db.getAllPuppies);
+router.get("/puppies/:id", db.getSinglePuppy);
+router.post("/puppies", db.createPuppy);
+router.put("/puppies/:id", db.updatePuppy);
+router.delete("/puppies/:id", db.removePuppy);
 
 //Clients Routes
-router.get("/api/clients", Clients.getAllClients);
-router.get("/api/clients/:id", Clients.getSingleClient);
-router.post("/api/clients", Clients.createClient);
-router.put("/api/clients/:id", Clients.updateClient);
-router.delete("/api/clients/:id", Clients.removeClient);
+router.get("/clients", Clients.getAllClients);
+router.get("/clients/:id", Clients.getSingleClient);
+router.post("/clients", Clients.createClient);
+router.put("/clients/:id", Clients.updateClient);
+router.delete("/clients/:id", Clients.removeClient);
 
 export default router;
