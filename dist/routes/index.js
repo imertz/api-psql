1d3bc972f87f6d4df8c3e1d56da873629db2e2e6
Ioannis Mertzanis <jomertz@gmail.com>
2018-07-19 16:50:40 +0300

    FIxed route /api

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -21,17 +21,17 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var router = _express2.default.Router();
 
 // Puppies Routes
-router.get("/api/puppies", _queries2.default.getAllPuppies);
-router.get("/api/puppies/:id", _queries2.default.getSinglePuppy);
-router.post("/api/puppies", _queries2.default.createPuppy);
-router.put("/api/puppies/:id", _queries2.default.updatePuppy);
-router.delete("/api/puppies/:id", _queries2.default.removePuppy);
+router.get("/puppies", _queries2.default.getAllPuppies);
+router.get("/puppies/:id", _queries2.default.getSinglePuppy);
+router.post("/puppies", _queries2.default.createPuppy);
+router.put("/puppies/:id", _queries2.default.updatePuppy);
+router.delete("/puppies/:id", _queries2.default.removePuppy);
 
 //Clients Routes
-router.get("/api/clients", _Clients2.default.getAllClients);
-router.get("/api/clients/:id", _Clients2.default.getSingleClient);
-router.post("/api/clients", _Clients2.default.createClient);
-router.put("/api/clients/:id", _Clients2.default.updateClient);
-router.delete("/api/clients/:id", _Clients2.default.removeClient);
+router.get("/clients", _Clients2.default.getAllClients);
+router.get("/clients/:id", _Clients2.default.getSingleClient);
+router.post("/clients", _Clients2.default.createClient);
+router.put("/clients/:id", _Clients2.default.updateClient);
+router.delete("/clients/:id", _Clients2.default.removeClient);
 
 exports.default = router;
